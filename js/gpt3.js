d9b548197beb631828db2a1a15f5be79e5190498
0x00badcode <paul.crinquand@epitech.eu>
2023-03-01 21:46:30 +0100

    v1

---
+++ js/gpt3.js
@@ -0,0 +1,17 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion("text-davinci-003", {
+    prompt: "Write a blog post on vadala onions",
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+});
+
+console.log(response.choices[0].text);
